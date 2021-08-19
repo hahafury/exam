29c6841a77d9e4175ea84bddc23a804de260fa65
hahafury <74094674+hahafury@users.noreply.github.com>
2021-08-19 10:18:08 +0300

    add events

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -6,8 +6,6 @@ import styles from './LoginForm.module.sass';
 import FormInput from '../FormInput/FormInput';
 import Schems from '../../validators/validationSchems';
 import Error from '../Error/Error';
-import {getAllEvents} from '../../api/rest/restController';
-import jwt_decode from 'jwt-decode';
 
 class LoginForm extends React.Component {
   componentWillUnmount() {
